717c261aae1810884c7a7ae49c01204e4577a29b
Blake Strickland <Stricklandb02@gmail.com>
2015-10-28 10:40:26 -0400

    Class examplees

--- graph.js
+++ graph.js
@@ -1,1 +1,16 @@
+var scale = d3.scale.linear()
+      .domain([2000,2012])
+      .range([50,950]);
 
+var axis = d3.svg.axis()
+      .scale(scale)
+      .orient("bottom")
+      .ticks(13);
+
+var g = d3.select("g");
+
+g.call(axis);
+
+g.append("circle")
+      .attr("cx",scale(2005))
+      .attr("r",5)
